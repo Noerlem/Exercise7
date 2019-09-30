1e1ce01520f174e253384fbd36b8e524474915f8
Noerlem <32601261+Noerlem@users.noreply.github.com>
2019-09-30 11:02:04 +0200

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var udpClient = new Client(args);
+        }
+    }
+
+    public class Client
+    {
+        UdpClient _client = new UdpClient();
+
+        public Client(string[] args)
+        {
+            //Send command line argument over udb forbindelse
+            _client.Connect("10.0.0.1", 9000);
+
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(args[0]);
+
+            _client.Send(sendBytes, sendBytes.Length);
+
+
+            //Receive msg from server
+            IPEndPoint server = new IPEndPoint(IPAddress.Any, 9000);
+            Byte[] receivedBytes = _client.Receive(ref server);
+            string receivedString = Encoding.ASCII.GetString(receivedBytes);
+
+            Console.WriteLine("{0}", receivedString);
+
+        }
+
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server udpServer = new Server();
+        }
+    }
+    public class Server
+    {
+        public Server()
+        {
+            IPEndPoint IP = new IPEndPoint(IPAddress.Parse("10.0.0.1"), 9000);
+            UdpClient server = new UdpClient(IP);
+
+            //Receive data på port 9000
+            byte[] receivedBytes = new byte[124];
+            IPEndPoint receive = new IPEndPoint(IPAddress.Any, 9000);
+
+            receivedBytes = server.Receive(ref receive);
+            string ReceivedString = Encoding.ASCII.GetString(receivedBytes);
+            ReceivedString = ReceivedString.ToUpper();
+
+            switch (ReceivedString)
+            {
+                case "U":
+                {
+                    Console.WriteLine("U was Received");
+
+                    string file = File.ReadAllText("/proc/uptime");
+                    byte[] sendBytes = Encoding.ASCII.GetBytes(file);
+                    server.Send(sendBytes, sendBytes.Length,receive.Address.ToString(),9000);
+
+                    break;
+                }
+                case "L":
+                {
+                    Console.WriteLine("L was Received");
+
+                    string file = File.ReadAllText("/proc/loadavg");
+                    byte[] sendBytes = Encoding.ASCII.GetBytes(file);
+                    server.Send(sendBytes, sendBytes.Length, receive.Address.ToString(), 9000);
+
+                    break;
+
+                }
+                default:
+                    Console.WriteLine("Something else was received: {0}", ReceivedString);
+                    break;
+            }
+        }
+    }
+}
